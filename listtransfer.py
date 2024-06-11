7e9145d055abf081af1c17c524c70d1e65974119
Cairo Faleiros <cairofal@gmail.com>
2024-06-11 18:51:19 -0300

    udate

---
+++ listtransfer.py
@@ -0,0 +1,14 @@
+unconfirmed_users = ['Mary','Bob', 'Trinity'] 
+confirmed_users = []
+
+#Check users until there's no more users to fill the list
+
+while unconfirmed_users: 
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+    print("\nThe following users have been confirmed: \n") 
+    
+    for confirmed_user in confirmed_users: 
+        print(confirmed_user.title()) 
+
